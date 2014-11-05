909865874edcd31d681ba1223911fc8e685d7a54
oberask <liuhao3418@gmail.com>
2014-11-05 17:25:14 +0800

    rev passed

--- Reverse Int/Reverse Int/Reverse Int.cpp
+++ Reverse Int/Reverse Int/Reverse Int.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int reverse (int x){
-	if(x<0){
-
+	int count = 0;
+	int num = x;
+	while(num!=0){
+		num/=10;
+		count++;
 	}
+	int *p = new int[count];
+
+	for(int i = 0;i<count;i++){
+		p[i]=x%10;
+		x/=10;
+	}
+	
+	int rev = 0;
+	for(int m=0;m<count;m++)
+	rev += p[m]*pow((double)10,(double)count-m-1);
+	return rev;
+}
+
+void main()
+{
+	for(int i = 0; i<5;i++){
+		int x;
+		cin>>x;
+		cout<<reverse(x)<<endl;
+	}
+	//cout<<reverse(0)<<endl;
 }
