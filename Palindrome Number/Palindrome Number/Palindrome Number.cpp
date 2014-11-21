4730dde0bd9d93d3076c5291f9ecb421dd9d3e91
oberask <liuhao3418@gmail.com>
2014-11-21 15:13:22 +0800

    palindrome solution

--- Palindrome Number/Palindrome Number/Palindrome Number.cpp
+++ Palindrome Number/Palindrome Number/Palindrome Number.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 bool isPalindrome(int x) {
-        
+	if(x<0) return false;
+        int temp = x;
+		int num = 0;
+		int sum = 0;
+		while(temp!=0){
+			num=temp%10;
+			sum=sum*10+num;
+			temp/=10;
+		}
+		return x==sum;
     }
+
+void main(){
+	int x;
+	cin>>x;
+	if (isPalindrome(x))
+		cout<<"yes"<<endl;
+	else
+		cout<<"no"<<endl;
+}
