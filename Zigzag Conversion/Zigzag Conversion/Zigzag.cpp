d3f5f882730ed5bae4f58a20c8883360ec32c58a
oberask <liuhao3418@gmail.com>
2014-11-25 10:38:42 +0800

    Zigzag

--- Zigzag Conversion/Zigzag Conversion/Zigzag.cpp
+++ Zigzag Conversion/Zigzag Conversion/Zigzag.cpp
@@ -4,21 +4,36 @@ using namespace std;
 
 string convert(string s, int nRows) {
 	string zig = "";
-	for(int i = 0;i<nRows;i++){
-		for(int j = i; j<s.length();){
-			zig+= s[j];
-			if(i%2==0)
-				j+=(nRows+nRows/2);
-			else
-				j+=((nRows+nRows/2)/2);
+
+	if (nRows <= 1 || s.length() == 0)  
+           return s;
+
+	for(int i = 0; i<nRows;i++){
+		if(i==0||i==(nRows-1)){
+			for(int j = i;j<s.length();){
+				zig+=s[j];
+				j+=((nRows-1)*2);
+			}
 		}
+		else{
+			for(int j = i;j<s.length();){
+				zig+=s[j];
+				j+=((nRows-i-1)*2);
+				if(j<s.length()){
+					zig+=s[j];
+					j+=i*2;
+				}
+			}
+		}		
 	}
 	return zig;
 }
 
+
+
 void main(){
-	string s = "PAYPALISHIRING";
-	string zig = convert(s,4);
+	string s = "PAYPALISHIRINGFSDFAFGAGDAGFDGFHJUTYJUYKMUIKTRHRTH";
 	cout<<s<<endl;
+	string zig = convert(s,5);
 	cout<<zig<<endl;
 }
