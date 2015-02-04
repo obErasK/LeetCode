21674d2b5de16c14823d53d667518020d8a569ae
oberask <liuhao3418@gmail.com>
2015-02-04 14:47:42 +0800

    modify

--- Gas Station/stest.cpp
+++ Gas Station/stest.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 int main(){
-	enum week
-	{
-		mon,tue,wen,thur,fri
-	};
-	int a = 1;
-	if(a==mon)
-		cout<<"yes"<<endl;
-	else
-		cout<<"no"<<endl;
-	return 0;
+
+	cout<<<<endl;
 }
