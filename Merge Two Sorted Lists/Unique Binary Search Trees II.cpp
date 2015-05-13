5d28a80f93d3e69a60db4fc27f5436afeb6d43c0
oberask <liuhao3418@gmail.com>
2015-05-13 17:22:58 +0800

    bst

---
+++ Merge Two Sorted Lists/Unique Binary Search Trees II.cpp
@@ -0,0 +1,47 @@
+//Unique Binary Search Trees II 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+
+
+vector<TreeNode*> gTrees(int begin, int end) {
+		std::vector<TreeNode*> v;
+        if(begin>end) { v.push_back(NULL); return v; }
+
+        if(begin==end) {
+        	TreeNode *pNode = new TreeNode(begin);
+        	v.push_back(pNode);
+        	return v;
+        } 
+
+        std::vector<TreeNode*> vleft,vright;
+        for(int i = begin;i<=end;i++){
+        	vleft = gTrees(begin,i-1);
+        	vright = gTrees(i+1,end);
+
+        	for(int k = 0; k<vleft.size();k++){
+	        	for(int j = 0; j<vright.size();j++){
+	        		TreeNode* temp = new TreeNode(i);
+	        		temp->left = vleft.at(k);
+	        		temp->right = vright.at(j);
+	        	}
+	        }
+        }
+        return v;
+    }
+
+vector<TreeNode*> generateTrees(int n) {
+        return gTrees(1,n);
+    }
+
+    int main(){
+
+    }
